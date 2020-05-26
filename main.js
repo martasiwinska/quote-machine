534fa1ce5761186a23b098b36fadd43da300901a
Marta SIWINSKA <msiwinska@splio.com>
2020-05-27 00:03:30 +0200

    styles and functionality

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 // JavaScript Document
+var endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
+
+function getQuote(){
+	fetch(endpoint)
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			displayQuote(data.message);
+		})
+		.catch(function(){
+			console.log('An error occured');
+		});
+}
+
+function displayQuote (quote) {
+	var quoteText = document.querySelector('.quote-text');
+	quoteText.textContent = quote;
+}
+
+var newQuoteButton = document.querySelector('.new-quote');
+
+newQuoteButton.addEventListener('click', getQuote);
+
+getQuote();
